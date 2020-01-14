950fa5064933ebcdde71d651ecd1581204f629c8
booldook <booldook@gmail.com>
2020-01-14 14:44:51 +0900

    pagerMaker()

--- js/00.slide.js
+++ js/00.slide.js
@@ -6,7 +6,7 @@ var slides = [
 	{src: "./img/p5.png", link: "#"}
 ];
 
-function slideMaker(chk) {
+function slideMaker($parent, chk) {
 	var html = '';
 	for(var i in slides) {
 		html += '<div class="ban">';
@@ -15,19 +15,31 @@ function slideMaker(chk) {
 		html += '</a>';
 		html += '</div>';
 	}
-	$(".ban-wrap").html(html);
-	if(chk) $(".ban-wrap").append($(".ban-wrap > div").eq(0).clone());
+	$parent.find(".ban-wrap").html(html);
+	if(chk) $parent.find(".ban-wrap").append($parent.find(".ban-wrap > div").eq(0).clone());
+}
+
+function pagerMaker($parent) {
+	var html = '';
+	for(var i in slides) {
+		html += '<div class="pager">●</div>';
+	}
+	$parent.find(".pagers").html(html);
 }
 
 
 /* 무한반복형 */
 (function(){
-	slideMaker(true);
-	var $ban = $("#ban-wrap1");
-	var cnt = slides.length;
+	var $bans = $("#bans1");
+	var $ban = $bans.find(".ban-wrap");
+	var $prev = $bans.find(".prev");
+	var $next = $bans.find(".next");
+	var cnt = slides.length; //5
 	var now = 0;
 	var delay = 4000;
 	var speed = 300;
+	slideMaker($bans, true);
+	pagerMaker($bans);
 	var interval = setInterval(slide, delay);
 	function slide() {
 		if(now == cnt) {
@@ -40,4 +52,30 @@ function slideMaker(chk) {
 	function ani() {
 		$ban.stop().animate({"left": -now*100+"%"}, speed);
 	}
+	$ban.mouseenter(function(){
+		clearInterval(interval);
+	});
+	$ban.mouseleave(function(){
+		clearInterval(interval);
+		interval = setInterval(slide, delay);
+	});
+	$prev.click(function(){
+		if(now > 0) {
+			now--;
+			ani();
+		}
+	});
+	$next.click(function(){
+		if(now < cnt - 1) {
+			now++;
+			ani();
+		}
+	});
 })();
+
+/*
+var color = $("객체").css("color");
+$("객체").css("color", "#000");
+$("객체").css({"color": "#000", "padding": "12px"});
+$("객체").animate({"color": "#000"}, 500);
+*/
